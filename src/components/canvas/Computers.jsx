f7d711aeb4edae582e7429ff234336b49c982bba
Daevah Taylaran <daevtayl@gmail.com>
2023-04-05 09:39:51 -0700

    finish main template

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./planet/scene.gltf");
+  const computer = useGLTF("./cat/scene.gltf");
 
   return (
     <mesh>
@@ -21,9 +21,9 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 2.0 : 2.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -1.95, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.8 : 1.0}
+        position={isMobile ? [0, -3, -2.2] : [2, -3.7, -2.5]}
+        rotation={[0.00, 0.9, -0.1]}
       />
     </mesh>
   );
